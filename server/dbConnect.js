0d0e769b1655af82a0319d686a2c130a60607856
Akhilesh Sharma <akhileshsharmask@gmail.com>
2024-05-14 16:54:47 +0530

    fix bugs

--- server/dbConnect.js
+++ server/dbConnect.js
@@ -1,6 +1,7 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.dbkey)
-    
-.then(() => console.log("database is connected..."))  
-.catch((error)=>console.log(error))
+mongoose
+  .connect("mongodb://localhost:27017/righttofashion")
+
+  .then(() => console.log("database is connected..."))
+  .catch((error) => console.log(error));
