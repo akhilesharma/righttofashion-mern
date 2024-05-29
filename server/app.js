699c869a306c5da9a46f1d20c0b9c323e08a8f8d
Akhilesh Sharma <akhileshsharmask@gmail.com>
2024-05-29 11:52:07 +0530

    add new chnage in both

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
-require("./dbConnect");
-const indexRoute = require("./route/index.route");
+const cors = require("cors");
 dotenv.config();
+require("./config/dbConnect");
+const indexRoute = require("./route/index.route");
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use("/", indexRoute);
 app.set(express.static(path.join(__dirname, "public")));
 app.use("/public", express.static("public"));
@@ -14,6 +16,10 @@ app.use("/public", express.static("public"));
 // app.get("*", (req, res) => {
 //   res.sendFile(path.join(__dirname, "./build/index.html"));
 // });
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Righttofashion Api Working Perfect...</h1>`);
+});
 
 let PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server is Running at PORT ${PORT}`));
--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+const DB_URI = process.env.DB_URI;
+const DB_NAME = process.env.DB_NAME;
 
 mongoose
-  .connect("mongodb://localhost:27017/righttofashion")
+  .connect(`${DB_URI}/${DB_NAME}`)
 
   .then(() => console.log("database is connected..."))
   .catch((error) => console.log(error));
